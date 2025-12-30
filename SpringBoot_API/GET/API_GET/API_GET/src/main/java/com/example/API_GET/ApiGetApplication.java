7b420ef9de603a2029ebcbe93f09c510b678a37e
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-12-30 18:58:40 -0300

    Fixing some issues

--- SpringBoot_API/GET/API_GET/API_GET/src/main/java/com/example/API_GET/ApiGetApplication.java
+++ SpringBoot_API/GET/API_GET/API_GET/src/main/java/com/example/API_GET/ApiGetApplication.java
@@ -1,29 +1,13 @@
 package com.example.API_GET;
 
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.API_GET.model.product;
-
-@RestController
-@RequestMapping("/api/produtos") // Rpta para acessar a API
-
+@SpringBootApplication
 public class ApiGetApplication {
-	@GetMapping
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetApplication.class, args);
 	}
-	public List<product> getAllProdutos(){
-		return Arrays.asList(
-				new product("Creme de Leite", 9.99, 10),
-				new product("Leite Integral", 8.99, 9),
-				new product("Ervilha em Lata", 3.99, 15),
-				new product("Milho em Lata", 4.99, 14),
-				new product("Refrigerante de Laranja 2L", 7.99, 4)
-		);
-	}
+
 }
